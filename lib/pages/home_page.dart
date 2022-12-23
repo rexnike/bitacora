500ad46e177db9b943f7de8236f06999ed2305d3
Hector Felipee <rexnike12@gmail.comm>
2022-12-22 21:39:11 -0500

    segundo comit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -30,6 +30,42 @@ class HomePage extends StatelessWidget {
               "Obtener la Data",
               ),
             ),
+            ElevatedButton(onPressed: (){
+                taskReference.add(
+                  {
+                    "title": "Ir de compras al super",
+                    "description": "Debemos comprar comida para todo el mes",
+                  },
+                  ).then((DocumentReference value){
+                  print(value.id);
+                }).catchError((error){
+                  print("Ocurrio un error en la registro");
+                }).whenComplete((){
+                  print("El registro ha terminado");
+                });
+            }, 
+            child: Text(
+              "Agregar documento",
+              ),
+            ),
+            ElevatedButton(onPressed: (){
+              taskReference
+              .doc("xyjFGskygECyUlxQoQtb")
+              .update({
+                "title": "Ir de paseo",
+                "description": "Tenemos que salir muy temprano",
+              },)
+              .catchError((error){
+                print(error);
+                },
+              ).whenComplete((){
+                print("Actualizacion Terminada");
+              });
+            }, 
+            child: Text(
+              "Actualizar doocumento",
+              ),
+            ),
           ],
         ),
       ),
