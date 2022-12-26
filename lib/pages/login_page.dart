9c363288274d6da5fb507b727832297e75f9ddad
Hector Felipee <rexnike12@gmail.comm>
2022-12-26 00:02:09 -0500

    veinti siete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,12 +2,14 @@
 import 'package:bitacora/pages/register_page.dart';
 import 'package:bitacora/ui/general/colors.dart';
 import 'package:bitacora/ui/widgets/button_custom_widget.dart';
-import 'package:bitacora/ui/widgets/button_normal_widget.dart';
 import 'package:bitacora/ui/widgets/general_widgets.dart';
 import 'package:bitacora/ui/widgets/textfield_normal_widget.dart';
 import 'package:bitacora/ui/widgets/textfield_password_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+import 'home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -18,9 +20,34 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
+  final formkey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  _login()async{
+    try{
+
+      if(formkey.currentState!.validate()){
+        UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: _emailController.text, 
+      password: _passwordController.text,
+        );
+
+      if(userCredential.user != null){
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => HomePage(),), (route) => false);
+        }
+      }
+
+    }on FirebaseAuthException catch(error){
+      if(error.code == "invalid-email"){
+        showSnackBarError(context, "El correo electronic es invalido");
+      }else if(error.code == "user-noot-found"){
+        showSnackBarError(context, "El usuario no esta registrado");
+      }else if(error.code == "worong-password"){
+        showSnackBarError(context, "La contraseña es incorrecta");
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,77 +56,82 @@ class _LoginPageState extends State<LoginPage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-
-              divider30(),
-              SvgPicture.asset('assets/images/uno.svg',
-              height: 180.0,
-              ),
-
-              divider10(),
-              Text("Inciar Sesion",
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w600,
-                color: kBrandPrimaryColor,
+          child: Form(
+            key: formkey,
+            child: Column(
+              children: [
+          
+                divider30(),
+                SvgPicture.asset('assets/images/uno.svg',
+                height: 180.0,
                 ),
-              ),
-
-              divider10(),
-              TextFieldNormalhWidget(
-                hintText: "Correo electronico", 
-                icon: Icons.email, 
-                controller: _emailController,
+          
+                divider10(),
+                Text("Inciar Sesion",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600,
+                  color: kBrandPrimaryColor,
+                  ),
                 ),
-
-              divider10(),
-              divider6(),
-              TextFieldPasswordWidget(
-                controller: _passwordController,),
-
-              divider20(),
-              ButtomCustomWidget(
-                text: "Iniciar Sesion",
-                icon: "check2",
-                color: kBrandPrimaryColor,
-                onPressed: (){},
-              ),
-              
-              divider20(),
-              Text("O ingrese con su correo"),
-
-              divider20(),
-              ButtomCustomWidget(
-                text: "Iniciar Sesion con Google",
-                icon: "google",
-                color: Color(0xfff84b2a),
-                onPressed: (){},
-              ),
-
-              divider20(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "¿Aun no estas registrado?"
-                    ),
-
-                  divider10width(),
-                  InkWell(
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=> RegisterPage(),),);
-                    },
-                    child: Text("Registrate",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: kBrandPrimaryColor,
-                    ),
+          
+                divider10(),
+                TextFieldNormalhWidget(
+                  hintText: "Correo electronico", 
+                  icon: Icons.email, 
+                  controller: _emailController,
+                  ),
+          
+                divider10(),
+                divider6(),
+                TextFieldPasswordWidget(
+                  controller: _passwordController,),
+          
+                divider20(),
+                ButtomCustomWidget(
+                  text: "Iniciar Sesion",
+                  icon: "check2",
+                  color: kBrandPrimaryColor,
+                  onPressed: (){
+                    _login();
+                  },
+                ),
+                
+                divider20(),
+                Text("O ingrese con su correo"),
+          
+                divider20(),
+                ButtomCustomWidget(
+                  text: "Iniciar Sesion con Google",
+                  icon: "google",
+                  color: Color(0xfff84b2a),
+                  onPressed: (){},
+                ),
+          
+                divider20(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "¿Aun no estas registrado?"
                       ),
-                    ),
-                ],
-              ),
-            ],
+          
+                    divider10width(),
+                    InkWell(
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=> RegisterPage(),),);
+                      },
+                      child: Text("Registrate",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: kBrandPrimaryColor,
+                      ),
+                        ),
+                      ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
